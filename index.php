db74a3b9e08e3be893c06eeddf4e2d5318047107
atef rebai <atef.rebai@esprit.tn>
2018-06-27 17:48:09 +0100

    init

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<?php
+echo gethostname();
+$ip= getenv('BACK_PY_SERVICE_HOST');
+$port== getenv('BACK_PY_SERVICE_PORT');
+echo $ip:$port
+?>
+
+<input name="btn" id="btn" type="button" OnClick="fn()"></input>
+<div id="div"></div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+<script>
+function fn(){
+<?php
+echo "$.get( '".$ip:$port."/', function( data ) {$( 'div' ).html( data );});"
+?>
+}
+
+</script>
+
+
+
+
+?>
